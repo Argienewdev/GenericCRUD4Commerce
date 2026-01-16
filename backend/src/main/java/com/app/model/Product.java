0ae3d843143e0a3258fea89e0094a6fced7527e7
Santiago Salamanca <salamancasantiago_2004@outlook.com>
2026-01-16 15:49:44 -0300

    feat: added clients and products

---
+++ backend/src/main/java/com/app/model/Product.java
@@ -0,0 +1,21 @@
+package com.app.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    public String name;
+    public String description;
+    public Double price;
+    public int stock;
+}
