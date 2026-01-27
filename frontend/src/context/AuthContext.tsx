be86d899e52b55f224c608cda4bfc172ff1b34c2
agustin <a.agustinpaul@gmail.com>
2026-01-26 21:50:04 -0300

    fix: add setLoading(true) when AuthProvider is mounted

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -22,11 +22,12 @@ export function AuthProvider({ children }: { children: ReactNode }) {
   }, []);
 
   const checkAuth = async () => {
+    setLoading(true);
     try {
       const response = await authService.me();
       setUser(response.user);
     } catch (error) {
-			console.log("Error inesperado al obtener datos del usuario: ", error);
+      console.log("Error inesperado al obtener datos del usuario: ", error);
       setUser(null);
     } finally {
       setLoading(false);
